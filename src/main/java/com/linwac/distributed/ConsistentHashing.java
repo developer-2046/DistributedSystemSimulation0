a5069d61a8803cff64902d7eed3d34f93d26b7d7
Yuvraj <yuvrajmalik2046@gmail.com>
2024-09-03 14:29:20 -0600

    first commit

--- src/main/java/com/linwac/distributed/ConsistentHashing.java
+++ src/main/java/com/linwac/distributed/ConsistentHashing.java
@@ -3,43 +3,43 @@ package com.linwac.distributed;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
-import static java.util.Objects.hash;
-
-public class consistentHashing {
+public class ConsistentHashing {
 
     private final SortedMap<Integer, Node> ring = new TreeMap<>();
     private final int replicaNumbers;
-    public consistentHashing(int replicaNumbers) {
+
+    public ConsistentHashing(int replicaNumbers) {
         this.replicaNumbers = replicaNumbers;
     }
-    public void addNode(Node node){
-        for (int i = 0; i < replicaNumbers; i++){
+
+    public void addNode(Node node) {
+        for (int i = 0; i < replicaNumbers; i++) {
             int hash = hash(node.getNodeId() + i);
             ring.put(hash, node);
         }
-
     }
+
     public void removeNode(Node node) {
         for (int i = 0; i < replicaNumbers; i++) {
             int hash = hash(node.getNodeId() + i);
             ring.remove(hash);
         }
     }
-    public Node getNode(String key){
+
+    public Node getNode(String key) {
         if (ring.isEmpty()) {
             return null;
         }
-
 
         int hash = hash(key);
         if (!ring.containsKey(hash)) {
             SortedMap<Integer, Node> tailMap = ring.tailMap(hash);
             hash = tailMap.isEmpty() ? ring.firstKey() : tailMap.firstKey();
         }
-            return ring.get(hash);
+        return ring.get(hash);
     }
+
     private int hash(String key) {
         return key.hashCode() & 0x7fffffff; // Ensures non-negative hash value
     }
-
 }
